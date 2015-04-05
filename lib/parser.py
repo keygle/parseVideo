5b56d4029aec33b1f9872d87e83140bd4ae45ec3
sceext <sceext@foxmail.com>
2015-04-05 12:26:56 +0800

    lib/parser.py add extractors list, will dynamic import modules in python

---
+++ lib/parser.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# parser.py, part for parse_video
+# 
+
+# import modules
+
+# global vars
+extractor_path = './extractors'
+extractor_list = {	# extractor list of modules and module name
+	'iqiyi': 'iqiyi', 
+}
+
+# classes
+
+# functions
+
+# end parser.py
+
+
