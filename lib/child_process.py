f96506965fe6469fcc2b35b4b8659fa46969f6a9
sceext <sceext@foxmail.com>
2015-04-05 15:23:00 +0800

    lib/base.py, cget, http get by curl, finished, ready to test

--- lib/child_process.py
+++ lib/child_process.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
-# base.py, part for parse_video
-# base: base part of parse_video. 
+# child_process.py, part for evdh : EisF Video Download Helper, sceext <sceext@foxmail.com> 2009EisF2015, 2015.04 
+# child_process: like child_process module in node.js, used to create child processes. 
+# version 0.0.3.0 test201504051522 (public version)
 # author sceext <sceext@foxmail.com> 2015.04 
 # copyright 2015 sceext 
 #
 # This is FREE SOFTWARE, released under GNU GPLv3+ 
 # please see README.md and LICENSE for more information. 
 #
-#    parse_video : parse videos from many websites. 
+#    evdh : EisF Video Download Helper, auto download videos on web pages. 
 #    Copyright (C) 2015 sceext <sceext@foxmail.com> 
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -22,33 +23,24 @@
 #
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 
-# import modules
-from . import child_process
-
-# global config
-
-USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64; rv:37.0) Gecko/20100101 Firefox/37.0'
-
-# classes
+# require import modules
+import subprocess
 
 # functions
 
-# cget, http get by curl
-def cget(url, user_agent=USER_AGENT, referer=None):
+def run(args, shell=False):
     
-    # make curl args
-    if referer != None:
-        curl_args = ['curl', '--user-agent', user_agent, '--referer', referer, url]
-    else:
-        curl_args = ['curl', '--user-agent', user_agent, url]
-    # just call curl
-    stdout, stderr = child_process.get_output(curl_args)
+    exit_code = subprocess.call(args, shell=shell);
+    return exit_code
+
+def get_output(args, shell=False):
     
-    # done
-    return stdout
+    p = subprocess.Popen(args, shell=shell, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    return p.communicate()
 
 
-# end base.py
+# end child_process.py
 
 
