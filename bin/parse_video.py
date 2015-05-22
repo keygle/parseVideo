85db44908be1613fadfd476c3cfdb5909f36010a
sceext <sceext@foxmail.com>
2015-05-22 13:10:15 +0800

    parse_video version 0.2.1.0 test201505221308, add command line process code from evparse, add --min --max --debug --fix-unicode options, test OK

--- bin/parse_video.py
+++ bin/parse_video.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # parse_video.py, part for parse_video : a fork from parseVideo. 
 # parse_video:bin/parse_video: parse_video main bin file. 
-# version 0.1.6.0 test201505221301
+# version 0.1.7.0 test201505221308
 # author sceext <sceext@foxmail.com> 2009EisF2015, 2015.05. 
 # copyright 2015 sceext
 #
@@ -46,7 +46,7 @@ def set_import(entry0, error0):
 
 # global config obj
 
-PARSE_VIDEO_VERSION = 'parse_video version 0.2.0.1 test201505142215'
+PARSE_VIDEO_VERSION = 'parse_video version 0.2.1.0 test201505221308'
 
 etc = {}
 etc['flag_debug'] = False
