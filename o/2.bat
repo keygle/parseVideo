749952057633302bd65b56ca98d60d3ca49037b9
sceext <sceext@foxmail.com>
2015-06-06 13:16:04 +0800

    update o/2.bat for --write-output-file, ready to test

--- o/2.bat
+++ o/2.bat
@@ -1,9 +1,13 @@
 @echo off
 echo parse_video ºÚµ•≤‚ ‘ 2.bat 0.py
+
+:start
 set /p url=«Î ‰»Î URL: 
 
-%1 parsev --output-easy --make-rename-list %url%
+%1 parsev --output-easy --write-output-file %url%
 
 pause
+
+goto start
 
 
