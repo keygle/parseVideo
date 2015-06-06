4aa3b72f30c827c30d51df1b3a14f96c30465da4
sceext <sceext@foxmail.com>
2015-06-06 17:47:53 +0800

    o/pvtkgui/gui.py, ready to test draw main window

--- o/pvtkgui/gui.py
+++ o/pvtkgui/gui.py
@@ -1,6 +1,6 @@
 # gui.py, part for parse_video : a fork from parseVideo. 
 # gui: o/pvtkgui/gui: parse_video Tk GUI, main gui file. 
-# version 0.0.1.0 test201506061728
+# version 0.0.2.0 test201506061747
 # author sceext <sceext@foxmail.com> 2009EisF2015, 2015.06. 
 # copyright 2015 sceext
 #
@@ -31,29 +31,90 @@ from tkinter.font import Font
 
 # global vars
 
+TEXT_MAIN_FONT_SIZE = 16	# 16px
+MAIN_BUTTON_TEXT = 'Start prase'
+
 # functions
 
+# class
+
+# main window
+
+class MainWin(object):
+    
+    def __init__(self):
+        self.root = None	# root window
+        
+        self.text_entry = None	# main Entry, input URL
+        self.button = None	# main Button, start parse
+        self.text_main = None	# main Text area, show text
+        
+        self.callback_main_button = None	# main button on click calback function
+        
+        pass
+    
+    # when main button be clicked, this will be callback
+    def _main_button_on_click(self):
+        pass
+    
+    # start create and show main window
+    def start(self):
+        # create root window
+        root = Tk()
+        self.root = root
+        
+        # create font
+        f = Font(root, size=TEXT_MAIN_FONT_SIZE)
+        
+        # create frame
+        f0 = Frame(root)	# top part, url text Entry and main Button
+        f1 = Frame(root)	# bottom part, main Text and scrollbars
+        f0.pack(side=TOP, fill=X, expand=False)
+        f1.pack(side=BOTTOM, fill=BOTH, expand=True)
+        
+        # create top part
+        # main button
+        b = Button(f0, font=f, command=self._main_button_on_click, text=MAIN_BUTTON_TEXT)
+        # main entry
+        e = Entry(f0, font=f)
+        # pack it
+        b.pack(side=RIGHT, fill=NONE, expand=False)
+        e.pack(side=LEFT, fill=X, expand=True)
+        # save objs
+        self.button = b
+        self.text_entry = e
+        
+        # create bottom part
+        # create scrollbar
+        sx = Scrollbar(f1, orient=HORIZONTAL)
+        sy = Scrollbar(f1, orient=VERTICAL)
+        # main Text area
+        t = Text(f1, wrap=NONE, font=f)
+        self.text_main = t
+        # pack it
+        sy.pack(side=RIGHT, fill=Y)
+        sx.pack(side=BOTTOM, fill=X)
+        t.pack(side=TOP, fill=BOTH, expand=True)
+        # config Text and Scrollbar
+        sx.config(command=t.xview)
+        sy.config(command=t.yview)
+        t.config(xscrollcommand=sx.set)
+        t.config(yscrollcommand=sy.set)
+        
+        # create main window done
+    
+    # start main loop
+    def mainloop(self):
+        # just start main loop
+        mainloop()
+    
+    # end MainWin class
+
 def test1():
-    root = Tk()
-    
-    f = Font(root, size=16)
-    
-    sx = Scrollbar(root, orient=HORIZONTAL)
-    sy = Scrollbar(root, orient=VERTICAL)
-    t = Text(root, wrap='none', font=f)
-    
-    sy.pack(side=RIGHT, fill=Y)
-    sx.pack(side=BOTTOM, fill=X)
-    t.pack(side=TOP, fill=BOTH, expand=True)
-    
-    sx.config(command=t.xview)
-    sy.config(command=t.yview)
-    t.config(xscrollcommand=sx.set)
-    t.config(yscrollcommand=sy.set)
-    
-    mainloop()
-    
-    pass
+    w = MainWin()
+    w.start()
+    w.mainloop()
+    # test done
 
 # start test
 if __name__ == '__main__':
