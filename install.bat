ec8657602348bee4e733bd98a198aa0b74c4fd81
sceext <sceext@foxmail.com>
2015-06-09 21:35:21 +0800

    add install.bat for auto install comtypes on windows

---
+++ install.bat
@@ -0,0 +1,7 @@
+
+@title parse_video install.bat: parse_video 迅雷 下载支持 安装
+@echo parse_video install.bat: 正在安装 迅雷 下载支持 组件, 请不要关闭此窗口
+pip install comtypes
+@echo parse_video install.bat: 安装已完成
+pause
+
