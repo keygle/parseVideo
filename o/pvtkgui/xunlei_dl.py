4358e012cdfa2d41dcaef20bda9f3794a8f553b4
sceext <sceext@foxmail.com>
2015-06-09 23:55:54 +0800

    fix auto reload comtypes failed, retry 1

--- o/pvtkgui/xunlei_dl.py
+++ o/pvtkgui/xunlei_dl.py
@@ -1,6 +1,6 @@
 # xunlei_dl.py, part for parse_video : a fork from parseVideo. 
 # xunlei_dl: o/pvtkgui/xunlei_dl: parse_video Tk GUI, add download tasks to xunlei with windows com ThunderAgent. 
-# version 0.0.4.0 test201506092339
+# version 0.0.4.1 test201506092355
 # author sceext <sceext@foxmail.com> 2009EisF2015, 2015.06. 
 # copyright 2015 sceext
 #
@@ -64,9 +64,12 @@ INSTALL_COMTYPES_BIN = 'install.bat'
 def import_cc():
     global cc
     try:
+        # try to reload comtypes, after installed comtypes
+        import comtypes
+        imp.reload(comtypes)
+        
+        # do import cc
         import comtypes.client as cc0
-        # auto try to reload
-        imp.reload(cc0)
     except Exception as e:
         raise ComTypesError(e)
     # done
