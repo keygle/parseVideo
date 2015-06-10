8bb4c7cfce071c352e39a16800c1ae1d19395800
sceext <sceext@foxmail.com>
2015-06-10 14:06:37 +0800

    pvtkgui fix xunlei install BUG, ready to test

--- o/pvtkgui/xunlei_dl.py
+++ o/pvtkgui/xunlei_dl.py
@@ -1,6 +1,6 @@
 # xunlei_dl.py, part for parse_video : a fork from parseVideo. 
 # xunlei_dl: o/pvtkgui/xunlei_dl: parse_video Tk GUI, add download tasks to xunlei with windows com ThunderAgent. 
-# version 0.0.9.0 test201506101346
+# version 0.0.10.0 test201506101406
 # author sceext <sceext@foxmail.com> 2009EisF2015, 2015.06. 
 # copyright 2015 sceext
 #
@@ -59,7 +59,7 @@ class CreateComObjError(XunleiDlError):
 # global vars
 cc = None	# comtypes.client
 
-INSTALL_COMTYPES_BIN = 'o/install.bat'
+INSTALL_COMTYPES_BIN = '.\\o\\install.bat'
 
 # functions
 
@@ -141,6 +141,7 @@ def install_comtypes():
     # just run install.bat to install comtypes
     run_sub.run_sub([INSTALL_COMTYPES_BIN], shell=True)
     # auto reload comtypes after install
+    import comtypes
     imp.reload(comtypes)
     # done
 
