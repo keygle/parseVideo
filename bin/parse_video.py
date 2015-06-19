35a3b275e2c620582dd875fc10328f5742d05c37
sceext <sceext@foxmail.com>
2015-06-19 21:19:45 +0800

    parse_video version 0.2.8.0 fix iqiyi with node_port, test OK

--- bin/parse_video.py
+++ bin/parse_video.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # parse_video.py, part for parse_video : a fork from parseVideo. 
 # parse_video:bin/parse_video: parse_video main bin file. 
-# version 0.1.25.0 test201506181857
+# version 0.1.26.0 test201506192118
 # author sceext <sceext@foxmail.com> 2009EisF2015, 2015.06. 
 # copyright 2015 sceext
 #
@@ -48,7 +48,7 @@ def set_import(entry0, error0):
 
 # global config obj
 
-PARSE_VIDEO_VERSION = 'parse_video version 0.2.7.0 test201506151426'
+PARSE_VIDEO_VERSION = 'parse_video version 0.2.8.0 test201506192118'
 
 etc = {}
 etc['flag_debug'] = False
