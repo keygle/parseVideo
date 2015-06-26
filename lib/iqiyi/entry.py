f71b9d54da50b3165f73184a775de06dab129d52
sceext <sceext@foxmail.com>
2015-06-26 15:31:59 +0800

    parse_video support --set-min-parse finished, test OK

--- lib/iqiyi/entry.py
+++ lib/iqiyi/entry.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # entry.py, part for parse_video : a fork from parseVideo. 
 # entry: parse_video/lib/iqiyi 
-# version 0.1.15.0 test201506261514
+# version 0.1.16.0 test201506261530
 # author sceext <sceext@foxmail.com> 2009EisF2015, 2015.06. 
 # copyright 2015 sceext
 #
@@ -36,7 +36,7 @@ from . import get_base_info, get_video_info
 # global vars
 
 # version of this extractor
-THIS_EXTRACTOR_VERSION = 'parse_video lib/iqiyi version 0.1.7.0 test201506252017'
+THIS_EXTRACTOR_VERSION = 'parse_video lib/iqiyi version 0.2.0.0 test201506261530'
 
 # check supported, week check, not strong check
 RE_SUPPORT_URL = []
