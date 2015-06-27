d0884aea4231b5dea29432259910c313dedd77de
sceext <sceext@foxmail.com>
2015-06-27 15:15:15 +0800

    o/pvtkgui, support video list for 271, fininshed, test OK

--- o/pvtkgui/b/conf_default.py
+++ o/pvtkgui/b/conf_default.py
@@ -1,6 +1,6 @@
 # conf_default.py, part for parse_video : a fork from parseVideo. 
 # conf_default: o/pvtkgui/conf_default: parse_video Tk GUI, default config, and ui text. 
-# version 0.1.6.0 test201506271353
+# version 0.1.7.0 test201506271514
 # author sceext <sceext@foxmail.com> 2009EisF2015, 2015.06. 
 # copyright 2015 sceext
 #
@@ -121,7 +121,7 @@ main_win_init_text0_1 = [
 main_win_init_text1 = [
     ['h2', ' parse_video Tk GUI 2'], 
     ['red_bold', '          parse_video 图形界面\n'], 
-    ['gray', '          version 0.2.2.0 test201506232106'], 
+    ['gray', '          version 0.2.3.0 test201506271514'], 
 ]
 
 main_win_init_text2 = [
