c34b7c77d2d7a79bfd18db1da6cce69bd7368aa8
sceext <sceext@foxmail.com>
2015-07-01 15:45:23 +0800

    add o/easy_dl, easy download tool for parse_video, o/easy_dl/lib/parsev.py, finished, ready to test

---
+++ o/easy_dl/lib/entry.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+# entry.py, part for parse_video : a fork from parseVideo. 
+# entry:o/easy_dl/lib
+# version 0.0.0.1 test201507011445
+# author sceext <sceext@foxmail.com> 2009EisF2015, 2015.07. 
+# copyright 2015 sceext
+#
+# This is FREE SOFTWARE, released under GNU GPLv3+ 
+# please see README.md and LICENSE for more information. 
+#
+#    parse_video : a fork from parseVideo. 
+#    Copyright (C) 2015 sceext <sceext@foxmail.com> 
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# import
+
+from . import parsev
+from . import make_name
+from . import wget
+
+from . import conf
+from . import merge
+
+# global vars
+
+etc = {}
+
+etc['hd'] = 0	# video hd, quality, used for parsev --min hd --max hd
+
+etc['min'] = 0	# min index of files to download, used for parsev --min-i
+etc['max'] = 0	# max index of files to download, used for parsev --max-i
+
+etc['out_dir'] = ''	# output file path
+
+# function
+
+# end entry.py
+
+
---
+++ o/easy_dl/lib/make_name.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# make_name.py, part for parse_video : a fork from parseVideo. 
+# make_name:o/easy_dl/lib
+# version 0.0.0.1 test201507011453
+# author sceext <sceext@foxmail.com> 2009EisF2015, 2015.07. 
+# copyright 2015 sceext
+#
+# This is FREE SOFTWARE, released under GNU GPLv3+ 
+# please see README.md and LICENSE for more information. 
+#
+#    parse_video : a fork from parseVideo. 
+#    Copyright (C) 2015 sceext <sceext@foxmail.com> 
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# import
+
+# function
+
+def make_raw_name(pinfo):
+    pass
+
+def make_i_name(raw_info, i=0):
+    pass
+
+# end make_name.py
+
+
+
