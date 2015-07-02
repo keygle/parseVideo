2a6c1a7e56041bfee5d3ea3cdf7def3f207c577e
sceext <sceext@foxmail.com>
2015-07-02 17:21:46 +0800

    update o/easy/make_name.py, comment

--- o/easy/make_name.py
+++ o/easy/make_name.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-# make_name.py, for parse_video Tk GUI, sceext <sceext@foxmail.com> 2009EisF2015, 2015.06 
+# make_name.py, for parse_video, sceext <sceext@foxmail.com> 2009EisF2015, 2015.07 
 
-# 说明: 此文件可用来更改 parse_video 图形界面 调用 迅雷 下载时, 生成的 文件名
+# 说明: 此文件可用来更改 parse_video 图形界面 和 猎影 插件支持部分 生成的 文件名
 
 # 下面这个 函数 "make()", 用于生成 文件名
 #	函数参数列表及其含义说明
@@ -29,18 +29,18 @@ def make(title, title_sub, title_no, title_short, quality, site, part_i, num_len
     # 下面生成 分段文件序号 数字, 最小数字长度 默认 是 4, 请修改下面一行
     part_i = num_len(part_i, 4)
     
-    # 下面一行, name 就是最终 生成的 文件名 的 主要部分
+    # 下面一行, main_name 就是最终 生成的 文件名 的 主要部分
     main_name = '_' + title_no + '_' + title + '_' + title_sub + '_' + quality + '_' + site
     
     # 下面一行, 为文件名添加 分段文件序号 和 扩展名
     name  = main_name + '_' + part_i + ext
     
-    # 上面的默认情况如下, 生成的文件名格式类似
+    # 上面的默认情况如下, 生成的文件名 (name) 格式类似
     # _0044_少年四大名捕未删减版第44集_决胜归来大团圆_0001.flv
     # _0002_花千骨第2集_灵虫糖宝初降世_0001.flv
     
     return name, main_name
 
-# end make_name.py, 2015-06-19 19:26 GMT+0800 CST
+# end make_name.py, last_update 2015-07-02 17:16 GMT+0800 CST
 
 
