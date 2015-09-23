9e0eac5bf6ed45b6e28c5ce60188bd2c0775c3f8
sceext <sceext@foxmail.com>
2015-09-23 20:25:29 +0800

    lib/var.py, base functions push() and pop(), test OK

--- lib/var.py
+++ lib/var.py
@@ -1,6 +1,25 @@
 # var.py, parse_video/lib
 # LICENSE GNU GPLv3+ sceext 
-# version 0.0.0.1 test201509231959
+# version 0.0.1.0 test201509232021
+
+# static data
+
+# var data
+_ = {}
+
+# save old var data for push() and pop()
+__old = []
+
+# var functions
+def init():
+    out = {}
+    return out
+
+def push():
+    __old.append(_)
+
+def pop():
+    return __old.pop()
 
 # end var.py
 
