dd2a1949192cc23927d889c9b4b634e542352f72
sceext <sceext@foxmail.com>
2015-09-24 02:00:39 +0800

    lib/e/__init__.py, call() function, partly finished

--- lib/e/__init__.py
+++ lib/e/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # __init__.py, parse_video/lib/e :: extractors' entry
 # LICENSE GNU GPLv3+ sceext 
-# version 0.0.2.0 test201509240149
+# version 0.0.3.0 test201509240200
 
 '''
 lib/e
@@ -10,6 +10,7 @@ lib/e/__init__.py
     extractors' entry for parse_video
 '''
 
+from .. import err
 from . import e_list
 
 def call(extractor_id, raw_url, raw_arg='', raw_method=''):
@@ -19,12 +20,32 @@ def call(extractor_id, raw_url, raw_arg='', raw_method=''):
         raw_url		raw_url to pass to the parse() function
         raw_arg		raw string after --extractor option's extractor_id
         		used to pass extractor args to the extractor
+        		default value is '', means no arg
         raw_method	raw string of --method option
         		used to pass method and method args to the extractor
+        		default value is '', means use the default method
     return what the extractor's parse() function return
     '''
+    # TODO import the extractor
+    ex = e_list.import_extractor(extractor_id)
     
-    pass
+    # init extractor's global var data
+    ex.var.push()
+    ex.var._ = ex.var.init()
+    ex.var._['raw_arg'] = raw_arg
+    ex.var._['raw_method'] = raw_method
+    # just call it
+    try:
+        result = ex.parse(raw_url)
+        return result
+    except err.ParseError as e:
+        # just raise it
+        raise
+    except Exception as e:
+        # TODO
+        raise err.UnknowError() from e
+    finally:	# recovery var data
+        ex.var._ = ex.var.pop()
 
 def get_list():
     '''
