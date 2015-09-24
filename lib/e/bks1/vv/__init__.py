fdf1bd35a1ff8125081d2546273c64dce609077f
sceext <sceext@foxmail.com>
2015-09-25 00:01:17 +0800

    lib/e/bks1/vv/__init__.py, finish load_conf() function

--- lib/e/bks1/vv/__init__.py
+++ lib/e/bks1/vv/__init__.py
@@ -1,13 +1,25 @@
 # __init__.py, parse_video/lib/e/bks1/vv
-# version 0.0.0.2 test201509242111
+# version 0.0.1.0 test201509250000
 
+from .... import b
 from .. import var
+
+from . ck_check import get_ck_token
 
 def load_conf():
     '''
     load vv config file
     '''
-    pass
+    # load main config file
+    mc = b.load_conf_file(var.CONF_FILE)
+    vv_conf_file = mc['vv_conf_file']
+    # load vv config file
+    vvc = b.load_conf_file(vv_conf_file)
+    # save conf info
+    var._['_vv_conf'] = vvc
+    var._['_qyid'] = vvc['qyid']
+    
+    return vvc
 
 # end __init__.py
 
