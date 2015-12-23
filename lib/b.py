2ba447ba96d45a6c087d69d8ce215606636acc0c
sceext <sceext@foxmail.com>
2015-12-23 17:07:42 +0800

    lib/_b/network.py, almost finished

--- lib/b.py
+++ lib/b.py
@@ -2,6 +2,15 @@
 
 import hashlib
 
+from ._b.network import (
+    dl_blob, 
+    dl_html, 
+    dl_json, 
+    dl_xml, 
+    post, 
+    post_form, 
+)
+
 # md5_hash
 def md5_hash(raw):
     return hashlib.md5(bytes(raw, 'utf-8')).hexdigest()
---
+++ lib/conf.py
@@ -0,0 +1,8 @@
+# conf.py, parse_video/lib/
+
+# firefox/43.0 on Windows 7 (64bit)
+DEFAULT_USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:43.0) Gecko/20100101 Firefox/43.0'
+
+# end conf.py
+
+
