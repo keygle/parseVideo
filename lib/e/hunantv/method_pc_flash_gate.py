822b6fa2ce7c5bca488a6cc13779af7078e33ede
sceext2 <sceext@foxmail.com>
2015-12-29 19:51:31 +0800

    lib/e/hunantv/, parse finished, ready to test

--- lib/e/hunantv/method_pc_flash_gate.py
+++ lib/e/hunantv/method_pc_flash_gate.py
@@ -27,10 +27,62 @@ def parse(method_arg_text):
     return out
 
 def _get_video_info(vid_info):
-    pass
+    # make first url
+    first_url = mango_tv3.gen_first_url(vid_info['vid'])
+    # [ OK ] log
+    log.o(log_text.method_got_first_url(first_url))
+    first = b.dl_json(first_url)
+    var._['_raw_first_json'] = first
+    # check code
+    if first['status'] != var.FIRST_OK_CODE:
+        raise err.MethodError(log_text.method_err_first_code(first['status'], var))
+    return common.parse_raw_first(first, _parse_raw_first_json)
+
+def _parse_raw_first_json(first):
+    data = first['data']
+    out = {}
+    # get base video info
+    info = data['info']
+    out['info'] = {}
+    out['info']['title'] = info['title']
+    out['info']['title_short'] = info['collection_name']
+    out['info']['title_sub'] = info['sub_title']
+    out['info']['title_no'] = b.simple_get_number_from_text(info['series'])
+    
+    # get video list
+    duration = float(info['duration'])
+    stream = data['stream']
+    out['video'] = []
+    for s in stream:
+        name = s['name']
+        one = {}
+        one['hd'] = var.TO_HD[name]
+        # set default values
+        one['format'] = 'm3u8'	# NOTE the type should be m3u8
+        one['size_px'] = [-1, -1]
+        one['size_byte'] = -1
+        one['time_s'] = duration
+        one['count'] = 1
+        # add one file info, the m3u8 file
+        one['file'] = []
+        f = {}
+        f['time_s'] = duration
+        f['url'] = mango_tv3.gen_before_url(s['url'])
+        one['file'].append(f)
+        out['video'].append(one)
+    return out
 
 def _get_file_urls(pvinfo):
-    pass
+    def worker(f, i):
+        raw = f['url']
+        before = b.dl_json(raw)
+        # check code
+        if before['status'] != var.BEFORE_OK_CODE:
+            raise err.MethodError('before code ' + b.str_or_str(before['status'] + ' is not ' + str(var.BEFORE_OK_CODE) + ' ')
+        f['url'] = before['info']	# update URL
+        return f
+    pool_size = var._['pool_size']['get_before']
+    return common.simple_get_file_urls(pvinfo, worker, msg='getting m3u8 file URLs', pool_size=pool_size)
 
 # end method_pc_flash_gate.py
 
