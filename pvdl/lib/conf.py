2ad1b2d07feaad981e9c93adf8db12291c765d37
sceext2 <sceext@foxmail.com>
2016-01-19 18:05:41 +0800

    pvdl/lib/conf.py, mark all unfinished features to TODO

--- pvdl/lib/conf.py
+++ pvdl/lib/conf.py
@@ -8,9 +8,9 @@ auto_select_hd = [-1, 8]
 output_dir = './dl/'	# --output
 
 # error retry times
-error_retry = 3		# -1 means retry forever; --retry
+error_retry = 3		# TODO -1 means retry forever; --retry
 # before error_retry, first sleep some time
-retry_wait_time_s = 1	# --retry-wait
+retry_wait_time_s = 1	# TODO --retry-wait
 
 
 
@@ -24,42 +24,44 @@ merge_output_format = 'mp4'	# NOTE other formats may not work
 # enable and disable pvdl features
 FEATURES = {	# --enable, --disable
     ## parse stage
-    'fix_size' : True, 	# if parse_video not get part file size, try to fix it here
-    'force_enable_more' : False, 	# each time use log file to enable_more
+    'fix_size' : True, 			# TODO if parse_video not get part file size, try to fix it here
+    'force_enable_more' : False, 	# TODO each time use log file to enable_more
     
     # create task
-    'check_log_file' : True, 	# check log file if exists, and stop if not match
-    'check_lock_file' : True, 	# use lock file to prevent two instance of pvdl to work in same directory at the same time
+    'check_log_file' : True, 		# TODO check log file if exists, and stop if not match
+    'check_lock_file' : True, 		# TODO use lock file to prevent two instance of pvdl to work in same directory at the same time
     
     ## download stage
     
     # before download
-    'check_remote_size' : False, 	# check network file size
+    'check_remote_size' : False, 	# TODO check network file size
+    'check_remote_fix_size' : True, 	# TODO if check_remote_size enabled, fix part file size here
+    
     'check_local_size' : True, 		# check local file size and skip finished files
-    'skip_local_lager_file' : False, 	# if local file is too large, just skip it (ignore Error)
+    'skip_local_lager_file' : False, 	# TODO if local file is too large, just skip it (ignore Error)
     
     # after download
-    'check_file_size' : True, 	# check part file size
-    'check_file_md5' : True, 	# check part file md5 checksum if possible
+    'check_file_size' : True, 		# check part file size
+    'check_file_md5' : True, 		# TODO check part file md5 checksum if possible
     
     ## merge stage
     'check_merged_size' : True, 	# check merged file size
-    'check_merged_time' : True, 	# check merged video time, if possible (requires mediainfo)
+    'check_merged_time' : True, 	# TODO check merged video time, if possible (requires mediainfo)
     
-    'force_merge' : False, 	# remove output file and continue merge, if output file already exists
+    'force_merge' : False, 		# TODO remove output file and continue merge, if output file already exists
     
     ## other options
-    'parse_twice' : True, 	# (TODO support it) first parse only to get video formats, second parse to get file URLs
-    'parse_twice_enable_more' : True, 	# enable_more to speed up second parse
-    'merge_single_file' : True, 	# do merge with single file (only one part file) (This is required for check_merged_time)
+    'parse_twice' : True, 		# TODO first parse only to get video formats, second parse to get file URLs
+    'parse_twice_enable_more' : True, 	# TODO enable_more to speed up second parse
+    'merge_single_file' : True, 	# TODO do merge with single file (only one part file) (This is required for check_merged_time)
     
-    'check_disk_space' : False, 	# before download to check disk space, and stop when space is not enough
-    'check_permission' : False, 	# check permission to write files
+    'check_disk_space' : False, 	# TODO before download to check disk space, and stop when space is not enough
+    'check_permission' : False, 	# TODO check permission to write files
     
-    'fix_unicode' : False, 	# fix_unicode is only for Windows
+    'fix_unicode' : False, 		# TODO fix_unicode is only for Windows
     
     ## DANGER options
-    'auto_remove_tmp_files' : False, 	# remove pvdl tmp files after download succeed; require enable check_merged_time and merge_single_file
+    'auto_remove_tmp_files' : False, 	# TODO remove pvdl tmp files after download succeed; require enable check_merged_time and merge_single_file
 }
 
 
@@ -69,8 +71,8 @@ CHECK_ERR_K = {	# NOTE -1 means unlimit
     'remote_size' : 1, 		# 1 %
     'remote_size_mb' : 64, 	# 64 MB
     # check_local_size
-    'local_size' : 1, 		# 1 %
-    'local_size_mb' : 64, 	# 64 MB
+    'local_size' : 0.1, 	# 0.1 %
+    'local_size_mb' : 0, 	# 0
     # check_file_size
     'file_size' : 1, 		# 1 %
     'file_size_mb' : 64, 	# 64 MB
