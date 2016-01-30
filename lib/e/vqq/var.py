c7c7635ceea89b6020677f49e93ff15d7f491485
sceext2 <sceext@foxmail.com>
2016-01-30 14:33:36 +0800

    [pv] re-write extractor, var.py, entry.py, finished

--- lib/e/vqq/var.py
+++ lib/e/vqq/var.py
@@ -2,50 +2,48 @@
 from .. import common
 from ... import conf
 
-# static data
-EXTRACTOR_ID = 'vqq'
-EXTRACTOR_NAME = 'vqq_1'
-SITE = 'v2q'
-SITE_NAME = '疼X视频'
-
-RE_SUPPORT_URL = [
-    '^http://v\.qq\.com/.+', 
-]
-
-METHOD_LIST = [
-    'pc_flash_gate', 
-]
-RE_VID_LIST = {	# NOTE not normal method
-    'title_short' : '\ntitle[ :]+"([^"]+)"', 
-    'title_sub' : '\nsecTitle[ :]+"([^"]+)"', 
-    'title_no' : '\ntitle[ :]+"([^"]+)"', 
-    'vid' : '\nvid[ :]+"([^"]+)"', 
-}
-# site fmt to parse_video hd quality
-TO_HD = {
-    'fhd' : 4, 		# id : 10209, 蓝光;(1080P)	1080p
-    'shd' : 2, 		# id : 10401, 超清;(720P)	720p
-    'hd' : 0, 		# id : 10412, 高清;(480P)	普清
+class Var(common.ExtractorVar):
+    # static data
+    EXTRACTOR_ID = 'vqq'
+    EXTRACTOR_NAME = 'vqq_1'
+    SITE = 'v2q'
+    SITE_NAME = '疼X视频'
     
-    # these low quality video, not well support now
-    'flv' : -1, 	# id : 1, 高清;(360P)		低清
-    'mp4' : -1, 	# id : 2, 高清;(360P)		低清
-    'sd' : -3, 		# id : 10203, 标清;(270P)	渣清
-}
-FIRST_OK_CODE = None	# TODO
-
-# base POST headers, NOTE may be no need
-BASE_POST_HEADER = {
-    'X-Requested-With' : 'ShockwaveFlash/20.0.0.228', 
-}
-
-DEFAULT_PLATFORM = 11
-DEFAULT_UTYPE = 0
-
-# var data
-class VarData(common.ExtractorVar):
-    def init(self):
-        out = super().init()
+    RE_SUPPORT_URL = [
+        '^http://v\.qq\.com/.+', 
+    ]
+    
+    METHOD_LIST = [
+        'pc_flash_gate', 
+    ]
+    RE_VID_LIST = {	# NOTE not normal method
+        'title_short' : '\ntitle[ :]+"([^"]+)"', 
+        'title_sub' : '\nsecTitle[ :]+"([^"]+)"', 
+        'title_no' : '\ntitle[ :]+"([^"]+)"', 
+        'vid' : '\nvid[ :]+"([^"]+)"', 
+    }
+    # site fmt to parse_video hd quality
+    TO_HD = {
+        'fhd' : 4, 		# id : 10209, 蓝光;(1080P)	1080p
+        'shd' : 2, 		# id : 10401, 超清;(720P)	720p
+        'hd' : 0, 		# id : 10412, 高清;(480P)	普清
+        
+        # these low quality video, not well support now
+        'flv' : -1, 	# id : 1, 高清;(360P)		低清
+        'mp4' : -1, 	# id : 2, 高清;(360P)		低清
+        'sd' : -3, 		# id : 10203, 标清;(270P)	渣清
+    }
+    _FIRST_OK_CODE = None	# TODO
+    
+    # base POST headers, NOTE may be no need
+    _BASE_POST_HEADER = {
+        'X-Requested-With' : 'ShockwaveFlash/20.0.0.228', 
+    }
+    
+    _DEFAULT_PLATFORM = 11
+    _DEFAULT_UTYPE = 0
+    
+    def _add_more_data(self, out):  # var data
         # config items
         out['pool_size']['get_formats'] = 8
         out['pool_size']['get_file_url'] = 16
@@ -67,8 +65,7 @@ class VarData(common.ExtractorVar):
         out['_limit_list'] = {}		# limit info, for fix_1080p
         return out
 # var exports
-var = VarData()
-_ = var._
+var = Var()
 # end var.py
 
 
