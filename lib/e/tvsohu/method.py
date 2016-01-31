7519b38e298d3fc3b67e04cf9c108ccbcfdb3f27
sceext2 <sceext@foxmail.com>
2016-01-31 21:22:01 +0800

    [pv] extractor tvsohu, rewrite, finished

--- lib/e/tvsohu/method.py
+++ lib/e/tvsohu/method.py
@@ -4,85 +4,90 @@ from ... import err, b
 from ...b import log
 from .. import common, log_text
 
-from . import var
+from .var import var
 from .o import main
 
-# support --more
-def get_raw_more(method_arg_text):
-    data_list = [	# NOTE --more mode to direct get vid_info
-        'vid_info', 
-        'raw_first_json', 	# NOTE also get raw first json info in --more mode
-    ]
-    raw_more = common.method_simple_check_use_more(var, method_arg_text, data_list)
-    if raw_more != None:	# set first_json
-        var._['_raw_first_json'] = raw_more['_data']['raw_first_json']
-    return raw_more
-
-def _do_get_vid_info():
-    raw_url = var._['_raw_url']
-    # INFO log
-    log.i(log_text.method_loading_page(raw_url))
-    raw_blob = b.dl_blob(raw_url)
-    raw_html_text = raw_blob.decode('utf-8', 'ignore')	# NOTE the html encoding is not 'utf-8'
-    var._['_raw_page_html'] = raw_html_text
+class Method(common.ExtractorMethod):   # common method class for extractor tvsohu
+    def _init_data(self):
+        self._more_data_list += [
+            'raw_first_json', 
+        ]
+        # NOTE for sub class
+        self._gen_one_before_url = None # should be a function
     
-    vid_info = common.parse_simple_get_vid_info(raw_html_text, var)
-    var._['_vid_info'] = vid_info
-    log.d(log_text.method_got_vid_info(vid_info))
-    return vid_info
+    def _gen_more_data(self):
+        out = super()._gen_more_data()
+        # add first_json info
+        out['raw_first_json'] = var._['_raw_first_json']
+        return out
+    
+    def _load_page(self, raw_url):
+        raw_blob = b.dl_blob(raw_url)
+        # NOTE the html encoding is not 'utf-8'
+        html_text = raw_blob.decode('utf-8', 'ignore')
+        return html_text
+    
+    def _get_video_info(self, vid_info):
+        # TODO support fast_parse here
+        # download all raw first jsons
+        raw_list = self._get_first_jsons(vid_info)
+        # do parse first json info
+        out = {}
+        out['video'] = []
+        for vid, raw in raw_list.items():
+            one = self._do_parse_first(raw, vid)
+            if not 'info' in out:
+                out['info'] = one['info']
+            out['video'].append(one['v'])
+        common.method_sort_video(out)
+        # NOTE count and select here
+        return common.method_simple_count_and_select(out, var)
+    
+    def _make_first_url(vid_info):
+        first_url = main.gen_first_url(vid_info['vid'])
+        log.o(log_text.method_got_first_url(first_url))
+        return first_url
+    
+    def _get_first_jsons(self, vid_info):
+        # check use_more to get first_json info
+        if var._['_use_more']:
+            more_data = var._['more']['_data']
+            var._['_raw_first_json'] = more_data['raw_first_json']
+            return var._['_raw_first_json']
+        # TODO support fast_parse here
+        # start download first jsons
+        first_url = self._make_first_url(vid_info)
+        first = b.dl_json(first_url)
+        
+        vid = vid_info['vid']
+        var._['_raw_first_json'][int(vid)] = first
+        # parse raw first, get other vids, and download other first info jsons
+        vid_list = self._do_parse_first(first, vid)['vid_list']
+        # make todo list
+        todo = []
+        for name, value in vid_list.items():	# NOTE vid == 0 is null
+            if (not int(value) in var._['_raw_first_json']) and (int(value) != 0):
+                one = {}
+                one['name'] = name
+                one['vid'] = value
+                one['url'] = main.gen_first_url(value)
+                todo.append(one)
+        pool_size = var._['pool_size']['get_first']
+        # INFO log
+        log.i('getting video info, count ' + str(len(todo)) + ', pool_size = ' + str(pool_size) + ' ')
+        result = b.map_do(todo, worker=_dl_one_first, pool_size=pool_size)
+        log.d('got video info (first) done ')
+        # save more raw_first_json
+        for r in result:
+            var._['_raw_first_json'][int(r['vid'])] = r['json']
+        # get first json info done
+        return var._['_raw_first_json']
+    
+    def _do_parse_first(self, first, vid):
+        return _do_parse_one_first(first, int(vid), self._gen_one_before_url)
+    # end Method class
 
-def get_vid_info():
-    # get vid_info from more if possible
-    vid_info = common.method_more_simple_get_vid_info(var, _do_get_vid_info)
-    return vid_info
-
-def check_enable_more(out):
-    # check enable_more
-    if var._['enable_more']:
-        out['_data'] = {}
-        out['_data']['vid_info'] = var._['_vid_info']
-        out['_data']['raw_first_json'] = var._['_raw_first_json']
-    return out
-
-# first part parse
-
-def get_video_info(vid_info, gen_one_before_url):
-    # check use_more
-    if not var._['_use_more']:
-        _get_first_json_info(vid_info, gen_one_before_url)
-    # do more parse
-    pvinfo = _get_video_info_2(gen_one_before_url)
-    return pvinfo
-
-def _get_first_json_info(vid_info, gen_one_before_url):
-    # TODO support fast_parse here
-    vid = vid_info['vid']
-    # make first URL
-    first_url = main.gen_first_url(vid)
-    # [ OK ] log
-    log.o(log_text.method_got_first_url(first_url))
-    first = b.dl_json(first_url)
-    var._['_raw_first_json'][int(vid)] = first
-    # parse raw first, get other vids, and download other first info jsons
-    vid_list = _parse_one_first(first, vid, gen_one_before_url)['vid_list']
-    # make todo list
-    todo = []
-    for name, value in vid_list.items():	# NOTE vid == 0 is null
-        if (not int(value) in var._['_raw_first_json']) and (int(value) != 0):
-            one = {}
-            one['name'] = name
-            one['vid'] = value
-            one['url'] = main.gen_first_url(value)
-            todo.append(one)
-    pool_size = var._['pool_size']['get_first']
-    # INFO log
-    log.i('getting video info, count ' + str(len(todo)) + ', pool_size = ' + str(pool_size) + ' ')
-    result = b.map_do(todo, worker=_dl_one_first, pool_size=pool_size)
-    log.d('got video info (first) done ')
-    # save more raw_first_json
-    for r in result:
-        var._['_raw_first_json'][int(r['vid'])] = r['json']
-    # get first json info done
+# base parse functions
 
 def _dl_one_first(info):
     log.d('getting first, name ' + b.str_or_str(info['name']) + ', vid ' + b.str_or_str(info['vid']) + ', url \"' + info['url'] + '\" ')
@@ -93,17 +98,11 @@ def _dl_one_first(info):
     out['json'] = raw
     return out
 
-def _parse_one_first(raw, vid, gen_one_before_url):
-    try:
-        return _do_parse_one_first(raw, int(vid), gen_one_before_url)
-    except err.PVError:
-        raise
-    except Exception as e:
-        raise err.MethodError('parse first info json failed ')
+# parse first info
 
 def _do_parse_one_first(raw, vid, gen_one_before_url):
     # check first code
-    if raw['status'] != var.FIRST_OK_CODE:
+    if raw['status'] != var._FIRST_OK_CODE:
         raise err.MethodError(log_text.method_err_first_code(raw['status'], var))
     out = {}
     data = raw['data']
@@ -136,7 +135,7 @@ def _do_parse_one_first(raw, vid, gen_one_before_url):
         raise err.MethodError('can not get vid_type from vid ' + b.str_or_str(vid) + ' ')
     v['hd'] = var.TO_HD[vid_name]
     # NOTE add quality here
-    quality = var.VID_NAME_LIST[vid_name]
+    quality = var._VID_NAME_LIST[vid_name]
     if quality != None:
         v['quality'] = quality
     # gen file before urls
@@ -155,23 +154,6 @@ def _do_parse_one_first(raw, vid, gen_one_before_url):
     out['v'] = v
     out['vid'] = vid
     return out
-
-def _get_video_info_2(gen_one_before_url):
-    # TODO support fast_parse here
-    raw_list = var._['_raw_first_json']
-    out = {}
-    out['video'] = []
-    for vid, raw in raw_list.items():
-        one = _parse_one_first(raw, vid, gen_one_before_url)
-        if not 'info' in out:
-            out['info'] = one['info']
-        out['video'].append(one['v'])
-    common.method_sort_video(out)
-    return out
-
-def count_and_select(pvinfo):
-    # TODO support fast_parse here
-    return common.method_simple_count_and_select(pvinfo, var)
 
 # end method.py
 
--- lib/e/tvsohu/method_pc_flash_gate.py
+++ lib/e/tvsohu/method_pc_flash_gate.py
@@ -1,38 +1,40 @@
 # method_pc_flash_gate.py, parse_video/lib/e/tvsohu/
 
 from ... import err, b
-from ...b import log
-from .. import common, log_text
+from .. import common
 
-from . import var, method
+from .var import var
+from . import method
 from .o import main
 
-# method_pc_flash_gate.parse(), entry function
-def parse(method_arg_text):
-    method.get_raw_more(method_arg_text)
-    # process method args
-    def rest(r):
+class Method(method.Method):
+    def _init_data(self):
+        super()._init_data()
+        # set gen_one_before_url
+        self._gen_one_before_url = _gen_one_before_url
+    
+    def _parse_arg_rest(self, r):
         if r == 'fast_parse':
             var._['flag_fast_parse'] = True
         elif r == 'gen_header':
             var._['flag_gen_header'] = True
-        else:	# unknow args
+        else:
             return True
-    common.method_parse_method_args(method_arg_text, var, rest)
-    vid_info = method.get_vid_info()
     
-    pvinfo = method.get_video_info(vid_info, _gen_one_before_url)
-    pvinfo = method.count_and_select(pvinfo)	# NOTE count and select
-    out = _get_file_urls(pvinfo)
-    # NOTE add headers
-    out = _add_headers(out)
-    out = method.check_enable_more(out)
-    return out
+    def _get_file_urls(self, pvinfo):
+        out = _do_get_file_urls(pvinfo)
+        out = _add_headers(out)
+        return out
+    # end Method class
+
+# base parse functions
 
 def _gen_one_before_url(su_i, vid, tvid, ch):
     return main.gen_before_url(su_i, vid, tvid, ch)
 
-def _get_file_urls(pvinfo):
+# get file urls
+
+def _do_get_file_urls(pvinfo):
     def worker(f, i):
         raw = b.dl_json(f['url'])
         try:
@@ -63,6 +65,9 @@ def _add_headers(pvinfo):
                     f['header']['Range'] = 'bytes=0-' + str(f['size'] - 1)
     return pvinfo
 
+# exports
+_method = Method(var)
+parse = _method.parse
 # end method_pc_flash_gate.py
 
 
