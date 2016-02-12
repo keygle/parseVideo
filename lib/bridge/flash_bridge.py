a8ef172c1f3f93ba344ca08ce95ce5273e4e572a
sceext2 <sceext@foxmail.com>
2016-02-12 13:47:25 +0800

    [bridge] will not use sandwich_bridge

---
+++ lib/bridge/flash_bridge.py
@@ -0,0 +1,22 @@
+# flash_bridge.py, parse_video/lib/bridge/
+# NOTE sandwich_bridge only works on Windows
+
+from .. import err, b, conf
+from ..b import log
+
+# TODO sandwich_bridge support
+
+# sandwich_bridge
+def init_sandwich_bridge(core):
+    
+    # TODO
+    pass
+
+def sandwich_call(raw):
+    
+    # TODO
+    pass
+
+# end flash_bridge.py
+
+
